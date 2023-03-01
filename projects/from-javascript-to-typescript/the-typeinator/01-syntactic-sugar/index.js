2d4a911b7f6ae5da54be6d3f81435d25e2ba37e5
iberis2 <snow9402@gmail.com>
2023-03-01 19:22:33 +0900

    js 옛날 문법 ES6 이후 문법으로 고쳐쓰기 완성

--- projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
@@ -1,4 +1,22 @@
 // Put your announceMachines function here! ✨
 // See ./original.js for its older JavaScript code.
 
+function announceMachines(announce, ...rest) {
+	let label;
+	let labelsCount = 0;
+
+	for (let machine of rest) {
+		if (machine.label) {
+			label = machine.label;
+			labelsCount += 1;
+		} else {
+			label = `Make: ${machine.make}; Model: ${machine.model}`;
+		}
+
+		announce(label);
+	}
+
+	return labelsCount;
+}
+
 module.exports.announceMachines = announceMachines;
