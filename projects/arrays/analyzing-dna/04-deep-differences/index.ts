d741e87d7b7007a715d637cb4cc50ac925504416
iberis2 <snow9402@gmail.com>
2023-03-19 21:22:02 +0900

    6장 배열 풀이

--- projects/arrays/analyzing-dna/04-deep-differences/index.ts
+++ projects/arrays/analyzing-dna/04-deep-differences/index.ts
@@ -1,2 +1,25 @@
-// Write your deepDifferences function here! ✨
-// You'll need to export it so the tests can run it.
+export function deepDifferences(a: string[][], b: string[][]) {
+	if (a.length !== b.length) {
+		return undefined;
+	}
+
+	let answer: (undefined | (string | undefined)[])[] = [];
+
+	for (let i = 0; i < a.length; i++) {
+		if (a[i].length !== b[i].length) {
+			answer.push(undefined);
+			continue;
+		}
+
+		let innerAnswer: (string | undefined)[] = [];
+		for (let j = 0; j < a[i].length; j++) {
+			if (a[i][j] !== b[i][j]) {
+				innerAnswer.push(undefined);
+			} else {
+				innerAnswer.push(a[i][j]);
+			}
+		}
+		answer.push(innerAnswer);
+	}
+	return answer;
+}
